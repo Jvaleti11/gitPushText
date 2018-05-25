7ceebdc69c8d94ce395b2a2f335e58a1ecf1bc27
Valentine Ogbeide <jvaleti11@gmail.com>
2018-05-25 20:14:01 +0930

    test-2

--- git_push.sh
+++ git_push.sh
@@ -3,7 +3,7 @@
   read -p "input commit message " msg
   if ((${#msg}!= 0))
   then
-    git add . && git commit -m "hello" && gco - && git pull && gco - && git merge master && git push
+    git add . && git commit -m "$msg" && gco - && git pull && gco - && git merge master && git push
     echo "merged and pushed"
   else
   echo "not merged and pushed"
