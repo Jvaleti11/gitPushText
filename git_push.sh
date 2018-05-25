35f3283ef171b22c312c79b9354d91fee330aa07
Valentine Ogbeide <jvaleti11@gmail.com>
2018-05-25 20:09:44 +0930

    first commit

---
+++ git_push.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+  read -p "input commit message " msg
+  if ((${#msg}!= 0))
+  then
+    git add . && git commit -m "hello" && gco - && git pull && gco - && git merge master && git push
+    echo "merged and pushed"
+  else
+  echo "not merged and pushed"
+  fi
